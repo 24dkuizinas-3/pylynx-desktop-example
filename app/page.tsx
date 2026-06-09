09bad95479ab1c3c9c61be5fe1aaba116bac7258
24dkuizinas-3 <24dkuizinas@winstanleyschool.org.uk>
2026-06-09 08:06:41 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,50 @@ export default function HomePage() {
           async
           src="https://pylynxosproject1.statuspage.io/embed/script.js"></script>
           
-                
-        
+                <Script
+        id="intercom-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(){
+              var w=window;
+              var ic=w.Intercom;
+              if(typeof ic==="function"){
+                ic('reattach_activator');
+                ic('update', w.intercomSettings);
+              }else{
+                var d=document;
+                var i=function(){i.c(arguments);};
+                i.q=[];
+                i.c=function(args){i.q.push(args);};
+                w.Intercom=i;
+                var l=function(){
+                  var s=d.createElement('script');
+                  s.type='text/javascript';
+                  s.async=true;
+                  s.src='https://intercom.io{APP_ID}';
+                  var x=d.getElementsByTagName('script')[0];
+                  x.parentNode.insertBefore(s,x);
+                };
+                if(document.readyState==='complete'){
+                  l();
+                }else{
+                  if(w.attachEvent){
+                    w.attachEvent('onload',l);
+                  }else{
+                    w.addEventListener('load',l,false);
+                  }
+                }
+              }
+            })();
+
+            window.intercomSettings = {
+              app_id: "${lazx153i}"
+            };
+            
+            window.Intercom('boot', window.intercomSettings);
+      
+        ></script>
       
       
         <script
